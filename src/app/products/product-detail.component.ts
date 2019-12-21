f6094f0a4a0effe6beb6dbaf17626da07b859058
desireadil <intentadil@gmail.com>
2019-12-22 03:22:22 +0530

    learn: route parameters

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
 
 import { Product } from './product';
 import { ProductService } from './product.service';
@@ -7,13 +9,20 @@ import { ProductService } from './product.service';
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
   pageTitle = 'Product Detail';
   product: Product;
   errorMessage: string;
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService, private route: ActivatedRoute) { }
 
+
+  ngOnInit(): void {
+
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.getProduct(id);
+
+  }
   getProduct(id: number) {
     this.productService.getProduct(id).subscribe({
       next: product => this.onProductRetrieved(product),
